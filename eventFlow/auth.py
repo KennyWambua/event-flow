94a3a4cac3a3bffb0b5e814c1618d87f505080df
Kenny W <KennyWambua>
2025-02-10 17:04:28 +0300

    changed the navbar

--- eventFlow/auth.py
+++ eventFlow/auth.py
@@ -5,4 +5,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html', active_page='login')
+    return render_template('login.html')
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
--- eventFlow/main.py
+++ eventFlow/main.py
@@ -9,3 +9,25 @@ def index():
 @main.route('/home')
 def home():
     return render_template('home.html', active_page='home')
+
+@main.route('/event/find')
+def findEvent():
+    return render_template('findEvent.html') 
+ 
+@main.route('/event/create')
+def createEvent():
+    return render_template('findEvent.html') 
+ 
+@main.route('/event/organizer')
+def contactOrganizer():
+    return render_template('findEvent.html') 
+ 
+@main.route('/help-center')
+def helpCenter():
+    return render_template('findEvent.html')
+ 
+@main.route('/event/tickets')
+def findMyTickets():
+    return render_template('findEvent.html')
+
+ 
