0d5b74e21cb97ba7c4fec1c0859ce58c243499f4
Kenny W <KennyWambua>
2025-02-14 12:11:14 +0300

    added image upload and url features

--- eventFlow/auth.py
+++ eventFlow/auth.py
@@ -26,7 +26,7 @@ def login():
         if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('main.home')
         
-        flash(f'Welcome back, {user.first_name}!', 'success')
+        flash(f'Welcome, {user.first_name}!', 'success')
         return redirect(next_page)
     
     return render_template('auth/login.html', form=form)
